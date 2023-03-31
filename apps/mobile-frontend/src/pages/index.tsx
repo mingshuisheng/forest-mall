80ffd6f66aab6f26ea873f44f2a320cb460ea943
mingshuisheng <1820924213@qq.com>
2023-03-31 17:21:48 +0800

    完成基础项目结构

---
+++ apps/mobile-frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import {Button} from "forest-mobile-ui"
+
+export default function Home() {
+  return (
+    <div>
+      <Button/>
+      <h1>Home</h1>
+    </div>
+  )
+}
---
+++ packages/mobile-ui/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react"
+
+export const Button: FC = () => {
+  return (
+    <button type="button" className="flex justify-center items-center text-2xl text-amber-300">button</button>
+  )
+}
